5b151b774038dd1ed37d0e025af4481d2e77198d
egbord <e.g.bord@inp.nsk.su>
2025-08-13 10:36:37 +0700

    Version 1.2.1, bug fixed

--- files_test/main.cpp
+++ files_test/main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 
 int main() {
-    int n;
-    std::cout << "Введите количество повторений: ";
-    std::cin >> n;
+    int n = 1;
+    while (n != 0) {
+	    std::cout << "Enter the number of repetitions (0 to exit): "; 
+	    std::cin >> n;
 
-    for (int i = 1; i <= n; ++i) {
-        std::cout << "Hello, World! #" << i << '\n';
+	    for (int i = 1; i <= n; ++i) {
+        	std::cout << "Hello, World! #" << i << '\n';
+    	}
     }
-
     return 0;
 }
