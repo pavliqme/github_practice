02dc61cc73aaba35ff5216cc4428d6bf8f5a37a9
gavrisenkoWORK <pasisenko@gmail.com>
2025-08-13 15:23:31 +0700

    Added EULA check

--- files_test/header.h
+++ files_test/header.h
@@ -2,6 +2,12 @@
 #include <cmath>
 #include <string>
 
+//EULA check
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+
 //sleep
 #include <chrono>
 #include <thread>
+
--- files_test/main.cpp
+++ files_test/main.cpp
@@ -1,16 +1,60 @@
 #include "header.h"
 
-int main() {
-	using namespace std::chrono_literals;
-	int n = 1;
-	while (n != 0) {
-		std::cout << "Enter the number of repetitions (0 to exit): "; 
-		std::cin >> n;
+int main(int argc, char* argv[]) {
+    std::string last_word;
 
-		for (int i = 1; i <= n; ++i) {
-        		std::cout << "Hello, World! #" << i << std::endl;
-			std::this_thread::sleep_for(500ms);
-    		}
-	}
-	return 0;
+    bool check_eula = true;
+    if (argc > 1) {
+        std::string arg = argv[1];
+        if (arg == "1") {
+            check_eula = false;
+            last_word = "true";
+        }
+    }
+
+    if (check_eula) {
+        std::ifstream file("EULA.txt");
+        if (!file.is_open()) {
+            std::cerr << "Error EULA-file reading\n";
+            return 1;
+        }
+
+        std::string line;
+        if (std::getline(file, line)) {
+            line.erase(0, line.find_first_not_of(" \t\n\r"));
+            line.erase(line.find_last_not_of(" \t\n\r") + 1);
+
+            std::istringstream iss(line);
+            std::string word;
+            while (iss >> word) {
+                last_word = word;
+            }
+
+            std::transform(last_word.begin(), last_word.end(), last_word.begin(),
+                           [](unsigned char c){ return std::tolower(c); });
+        } else {
+            std::cerr << "Empty EULA-file\n";
+            return 1;
+        }
+    }
+
+    if (last_word == "true" || last_word == "yes") {
+        std::cout << "EULA accepted. Running...\n";
+        using namespace std::chrono_literals;
+        int n = 1;
+        while (n != 0) {
+            std::cout << "Enter the number of repetitions (0 to exit): ";
+            std::cin >> n;
+
+            for (int i = 1; i <= n; ++i) {
+                std::cout << "Hello, World! #" << i << std::endl;
+                std::this_thread::sleep_for(500ms);
+            }
+        }
+    } else {
+        std::cout << "Accept EULA first! Exiting....\n";
+        return 1;
+    }
+
+    return 0;
 }
